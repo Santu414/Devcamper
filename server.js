a65773d522ea35e148eeef94cdb416b8988fb944
dileepnagankera <dileepnagankera@gmail.com>
2020-02-22 15:18:48 +0530

    Get all courses

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ connectDB();
 
 //load route files
 const bootcamps = require("./routes/bootcamps");
+const courses = require("./routes/courses");
 
 const app = express();
 
@@ -25,7 +26,7 @@ if (process.env.NODE_ENV === "development") {
 }
 
 //Mount Route Files
-app.use("/api/v1/bootcamps", bootcamps);
+app.use("/api/v1/courses", courses);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
